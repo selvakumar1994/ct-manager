cd2930f2dd7aa6c7562f6c63308feb5b0cac919f
selvakumar <selvakumar@cogzidel.com>
2018-06-18 17:49:16 +0530

    new project added--selva

---
+++ app/src/main/java/com/javiersantos/ml/async/DeleteDataInBackground.java
@@ -0,0 +1,49 @@
+package com.javiersantos.ml.async;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.javiersantos.ml.R;
+import com.javiersantos.ml.utils.UtilsApp;
+import com.javiersantos.ml.utils.UtilsDialog;
+import com.javiersantos.ml.utils.UtilsRoot;
+
+public class DeleteDataInBackground extends AsyncTask<Void, String, Boolean> {
+    private Context context;
+    private Activity activity;
+    private MaterialDialog dialog;
+    private String directory;
+    private String successDescription;
+
+    public DeleteDataInBackground(Context context, MaterialDialog dialog, String directory, String successDescription) {
+        this.context = context;
+        this.activity = (Activity) context;
+        this.dialog = dialog;
+        this.directory = directory;
+        this.successDescription = successDescription;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        Boolean status = false;
+
+        if (UtilsApp.checkPermissions(activity)) {
+            status = UtilsRoot.removeWithRootPermission(directory);
+        }
+
+        return status;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean status) {
+        super.onPostExecute(status);
+        dialog.dismiss();
+        if (status) {
+            UtilsDialog.showSnackbar(activity, successDescription, null, null, 2).show();
+        } else {
+            UtilsDialog.showTitleContent(context, context.getResources().getString(R.string.dialog_root_required), context.getResources().getString(R.string.dialog_root_required_description));
+        }
+    }
+}
